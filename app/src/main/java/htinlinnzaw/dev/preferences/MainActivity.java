5348b31a72f1e4ef0902ed83be910bd0d462a370
htinlinnzaw <htinlinnzaw.dev@gmail.com>
2020-10-19 13:40:13 +0630

    1. upgrade: miscellaneous fixes

--- app/src/main/java/htinlinnzaw/dev/preferences/MainActivity.java
+++ app/src/main/java/htinlinnzaw/dev/preferences/MainActivity.java
@@ -1,14 +1,35 @@
 package htinlinnzaw.dev.preferences;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity
+{
+    private static final String KEY = "key";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText edittext = findViewById(R.id.editText);
+        edittext.setOnEditorActionListener((textView, actionId, event) -> {
+            String value = textView.getText().toString();
+            save(KEY,value);
+            return false;
+        });
+        SharedPreferences sharedpreferences = getPreferences(MODE_PRIVATE);
+        String value = sharedpreferences.getString(KEY,"");
+        Log.e("Storing value",value);
+        edittext.setText(value);
+    }
+    public void save(String key,String value)
+    {
+        SharedPreferences sharedpreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(key,value);
+        editor.apply();
     }
 }
